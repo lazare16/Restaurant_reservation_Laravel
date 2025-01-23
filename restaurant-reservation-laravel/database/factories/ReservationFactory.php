3c76470ef320ef71ca53c80315df1044ad290311
lazare16 <76008900+lazare16@users.noreply.github.com>
2025-01-23 23:40:23 +0400

    Addition: adding factories

---
+++ restaurant-reservation-laravel/database/factories/ReservationFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class ReservationFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'user_id' => User::factory(), // Create a user
+            'table_number' => $this->faker->numberBetween(1, 50),
+            'reservation_date' => $this->faker->dateTimeBetween('now', '+1 month')->format('Y-m-d'),
+            'reservation_time' => $this->faker->time('H:i:s'),
+            'number_of_guests' => $this->faker->numberBetween(1, 10),
+            'special_request' => $this->faker->sentence,
+            'status' => $this->faker->randomElement(['pending', 'confirmed', 'canceled']),
+        ];
+    }
+}
